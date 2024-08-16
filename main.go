65b9de98867090da2718942885677fb2dd699244
porping <natthapon.nu@tnds.com>
2024-08-16 15:52:36 +0700

    feat: timeline breaking

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello develop")
+	fmt.Println("hello world")
+	a, b := 10, 2
+	output := devide(a, b)
+	fmt.Printf("a / b = %v / %v = %v", a, b, output)
+}
+
+func devide(a, b int) int {
+	result := a / b
+	return result
 }
